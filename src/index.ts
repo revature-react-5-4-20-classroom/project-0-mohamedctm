ec508369d90e47a16ecdfb2ecb392bdba5971981
mohamed Ahmed <giveintome100@gmail.com>
2020-05-22 15:48:52 -0400

    checking the connection

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import { reimbursementsRouter } from './Routers/reimbursements'
 import { loggingMiddleware } from './middleware/loggingMiddleware';
 import { sessionMiddleware } from './middleware/sessionMiddleware';
 import bodyParser from 'body-parser';
+import { PoolClient, QueryResult } from 'pg';
+import { connectionPool } from './repository';
+
+
 
 require('dotenv').config();
 
@@ -28,4 +32,12 @@ const port = process.env.port||3000
 
 app.listen(port, ()=>{
     console.log(`app listening port: ${port}`);
+    let client;
+         client =  connectionPool.connect();
+    if(client){
+        console.log(` connected to database ..`);
+    }else{
+        console.log(`not connected..`)
+    }
+
 })
