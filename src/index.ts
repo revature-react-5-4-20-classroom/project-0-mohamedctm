499cc1f2d511c3dc9000033cbd96c3a6cd97c3df
mohamed Ahmed <giveintome100@gmail.com>
2020-05-25 20:12:35 -0400

    nothing

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const app: Application = express();
 
 //checking webhokk
 app.get('/test',(req:Request, res:Response)=>{
-    res.send(' WebHook works');
+    res.send(' WebHook works beautifuly');
 
 });
 
