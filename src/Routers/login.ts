82d7d4b3f9181107a37315e7e51c68cf87f39875
mohamed Ahmed <giveintome100@gmail.com>
2020-05-29 10:25:07 -0400

    mohamed

--- src/Routers/login.ts
+++ src/Routers/login.ts
@@ -2,26 +2,20 @@ import express, { Router, Request, Response, NextFunction } from 'express';
 import {loguser} from '../repository/users';
 
 
-
-
-
 export const loginRouter: Router = express.Router();
-
-    loginRouter.get('/', (req, res)=>{
-    res.status(401).send(`invalid action`);
-    })
 
   loginRouter.post('/', async (req: Request, res: Response) => {
     const {username, password} = req.body;
     if(!username || !password) 
-    res.status(400).send('username and password are required fields for login');
+    res.status(400).json({"error":"username and password are required fields for login"});
       try {
         const userDetails = await loguser(username, password);
         if(req.session)  req.session.user = userDetails; // =>response with json(loged in userDetails)
         res.json(userDetails);
       } catch (e) {
         console.log(e.message);
-        res.status(401).send('invalid Credentials -- try again');
+        res.status(400).json({"error":"invalid Credentials"});
+
       } 
   }); 
 
