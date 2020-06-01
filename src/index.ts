cad3881d35fad841087e4189d97f545d157a7c5b
mohamed Ahmed <giveintome100@gmail.com>
2020-06-01 01:18:18 -0400

    sssSss

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const app: Application = express();
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", " Origin| *"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", " Origin"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
