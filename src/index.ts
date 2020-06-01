fc36594ff1abda2ba8154f39ccde7a59dec75719
mohamed Ahmed <giveintome100@gmail.com>
2020-06-01 01:21:29 -0400

    sssSsss

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { sessionMiddleware } from './middleware/sessionMiddleware';
 import bodyParser from 'body-parser';
 import { PoolClient, QueryResult } from 'pg';
 import { connectionPool } from './repository';
+var cors = require('cors');
 // import cors from 'cors';
 
 
@@ -21,11 +22,7 @@ const app: Application = express();
 
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", " Origin"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 app.use(bodyParser.json());
 app.use(sessionMiddleware);
 app.use(loggingMiddleware);
