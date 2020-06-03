83e71d695ef72f67d80f4502f1046b398cda75c6
mohamed Ahmed <giveintome100@gmail.com>
2020-06-03 02:57:10 -0400

    ll

--- src/Routers/users.ts
+++ src/Routers/users.ts
@@ -30,7 +30,7 @@ userRouter.get('/:id', async (req: Request, res: Response) => {
 
 userRouter.get('/', async (req: Request, res: Response) => {
   if(!req.session || !req.session.user) {
-    res.status(401).send('Please login here');
+    res.status(401).json('Please login here..');
   } else {
     const myrole = req.session.user.role;
       if(myrole === 'finance-manager') {
