30bf64d1b59a11d99992d68069def9f58f4d95a6
mohamed Ahmed <giveintome100@gmail.com>
2020-06-04 12:39:30 -0400

    playing with

--- src/Routers/logout.ts
+++ src/Routers/logout.ts
@@ -5,12 +5,13 @@ export const logoutRouter: Router = express.Router();
 
   logoutRouter.get('/', async (req: Request, res: Response) => {
     if (!req.session || !req.session.user) {
+        res.status(200).send('no session assigned');
+        
+    } else {
         req.session?.destroy(function() {
             res.clearCookie('connect.sid', { path: '/' });
             res.status(200).send('removed session');
         });
-    } else {
-        res.status(200).send('no session assigned');
     }
   }); 
 
