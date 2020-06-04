6e521627fec7d593e678135e0ba5fe6a033d6810
mohamed Ahmed <giveintome100@gmail.com>
2020-06-04 12:33:19 -0400

    oo

--- src/Routers/logout.ts
+++ src/Routers/logout.ts
@@ -4,8 +4,8 @@ import express, { Router, Request, Response, NextFunction } from 'express';
 export const logoutRouter: Router = express.Router();
 
   logoutRouter.get('/', async (req: Request, res: Response) => {
-    if (req.session && req.session.user.id !== null) {
-        req.session.destroy(function() {
+    if (!req.session || !req.session.user) {
+        req.session?.destroy(function() {
             res.clearCookie('connect.sid', { path: '/' });
             res.status(200).send('removed session');
         });
